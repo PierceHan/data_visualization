a87db7d9af4b6a353aee5980bd180ea306c5012b
Pierce3 <han2770@163.com>
2019-01-06 12:18:03 +0800

    feat:新建数据可视化项目

---
+++ src/main/java/com/zjp/DataVisualizationApplication.java
@@ -0,0 +1,14 @@
+package com.zjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataVisualizationApplication.class, args);
+	}
+
+}
+
